30291bf2ced59c897f8c06651fb539c609493384
Maigo Erit <maigo.erit@smit.ee>
2015-02-17 09:57:59 +0200

    cleanup

--- angular/client/app/settings/settings.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('angularDemoApp')
-  .controller('SettingsController', function ($scope, $http, $state, appConfig, $auth, SessionService) {
-  	$scope.user = SessionService.getCurrentUser();
-  
-  });
